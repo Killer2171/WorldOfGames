9947c2f28b255b0cc6430bcbbb8bb49319639c8b
Yaniv <yanivlevi7@gmail.com>
2021-11-26 17:56:46 +0200

    Scores and Utils

---
+++ Score.py
@@ -0,0 +1,12 @@
+
+def add_score(difficulty=3):
+        with open("./Scores.txt" , "r") as score_file:
+            score_file_read = score_file.readlines()
+            score_result = int(score_file_read[0])
+        total_score = (difficulty * 3) + 5 + score_result
+        with open("./Scores.txt" , "w") as score_file:
+             score_file.write(str(total_score))
+        return total_score
+print(add_score())
+
+# POINTS_OF_WINNING = 3 *
